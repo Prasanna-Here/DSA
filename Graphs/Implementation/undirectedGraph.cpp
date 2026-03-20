268e8e2843881572b151c60f528c39913fe6019d
Prasanna <prasannagithub1@gmail.com>
2026-03-20 21:43:55 +0530

    BFS Undirected Graph

--- Graphs/Implementation/undirectedGraph.cpp
+++ Graphs/Implementation/undirectedGraph.cpp
@@ -3,6 +3,25 @@
 #include <queue>
 using namespace std;
 
+void bfs(int start,vector<vector<int>> &adj,int n){
+    vector<int> vis(n+1,0);
+    queue<int> q;
+    q.push(start);
+    vis[start]=1;
+    
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        cout<<node<<" ";
+        for(auto neighbour:adj[node]){
+            if(!vis[neighbour]){
+                vis[neighbour]=1;
+                q.push(neighbour);
+            }
+        }
+    }
+}
+
 int main(){
     int n,m;
     cin>>n>>m;
@@ -13,7 +32,7 @@ int main(){
         adj[u].push_back(v);
         adj[v].push_back(u); //not  needed for directed graph
     }
-
+    
     //print adjacency list
     cout<<"adjacency list:"<<endl;
     for(int i=1;i<=n;i++){
@@ -23,4 +42,7 @@ int main(){
         }
         cout<<endl;
     }
+    
+    cout<<"BFS Traversal:"<<endl;
+    bfs(1,adj,n);
 }
